7d808bd889cd6a4a305f43b2e088badf53a1f2ae
csdhv9 <csdhv9@umsystem.edu>
2019-06-14 14:38:45 -0500

    Implement variable dimensions. Add docstring.

--- poincare_plot.py
+++ poincare_plot.py
@@ -1,20 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def poincare_plot(data):
-    X = []
-    Y = []
+def poincare_plot(data, DIMENSIONS=2):
+    """
+    Generate a poincare plot of the given time series.
 
-    for i in range(len(data) - 1):
-        X.append(data[i])
-        Y.append(data[i+1])
+    Parameters
+    ----------
+    data: list
+        Time series data.
+    DIMENSIONS: int, 2 or 3 if want visualized automatically.
+        Number of embedding dimensions.
 
-    plt.title("Poincare Plot")
-    plt.scatter(X, Y)
-    plt.show()
+    Returns
+    -------
+    Embedded data.
+    """
 
-    return X, Y
+    out = [[] for i in range(DIMENSIONS)]
+
+    for i in range(len(data) - DIMENSIONS + 1):
+        for offset in range(DIMENSIONS):
+            out[offset].append(data[i+offset])
+
+    if DIMENSIONS in [2, 3]:
+        fig = plt.figure()
+        ax = Axes3D(fig) if DIMENSIONS == 3 else fig.add_subplot(111)
+
+        ax.set_title("Poincare Plot")
+
+        for i in range(DIMENSIONS):
+            AXES = ['x', 'y', 'z']
+
+            getattr(ax, f'set_{AXES[i]}label')(f'f(t{"+ " + str(i) if i else ""})')
+
+        ax.scatter(*out)
+
+        plt.show()
+
+    return data
 
 
 if __name__ == '__main__':
